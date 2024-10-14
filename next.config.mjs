2ddaf307cf3486a444a7de02eae5d6f4911de159
sbin0819 <164972254+subin0819@users.noreply.github.com>
2024-10-14 17:23:18 +0900

    feat: firt commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
+import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 const nextConfig = {
   reactStrictMode: true,
 };
 
-export default nextConfig;
+export default createVanillaExtractPlugin(nextConfig);
