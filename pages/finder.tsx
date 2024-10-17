8da0aeb17ddfe4250f4883452a8208b898fb5673
sbin0819 <sbinha123@gmail.com>
2024-10-17 13:09:17 +0900

    fix: some work

---
+++ pages/finder.tsx
@@ -0,0 +1,1 @@
+export { FinderPage as default } from '@/pages/finder';
---
+++ src/pages/finder/ui/index.tsx
@@ -0,0 +1,10 @@
+export const FinderPage = () => {
+  return (
+    <div style={{ paddingTop: '100px' }}>
+      <h1>about page</h1>
+      <li>about</li>
+      <li>about2</li>
+      <li>about2</li>
+    </div>
+  );
+};
